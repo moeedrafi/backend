7797061240d808c5c1158c0cb5bbef415504c069
Moeed Rafi <moeedrafi95@gmail.com>
2025-03-17 17:31:32 +0500

    Connect to Mongo Atlas

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,23 +1,8 @@
-const express = require("express");
-const app = express();
-const port = process.env.PORT || 4000;
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+dotenv.config({
+  path: "./.env",
 });
 
-app.get("/api/jokes", (req, res) => {
-  const jokes = [
-    { id: 1, title: "A joke", content: "This is a joke" },
-    { id: 2, title: "A joke 2", content: "This is a joke 2" },
-    { id: 3, title: "A joke 3", content: "This is a joke 3" },
-    { id: 4, title: "A joke 4", content: "This is a joke 4" },
-    { id: 5, title: "A joke 5", content: "This is a joke 5" },
-  ];
-
-  res.send(jokes);
-});
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+connectDB();
